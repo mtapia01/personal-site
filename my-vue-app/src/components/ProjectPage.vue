b4aadb42595c2286d239da9195f768f7cb3f7602
mtapia01 <michaeltapia80@yahoo.com>
2025-03-01 22:51:52 -0800

    Init

---
+++ my-vue-app/src/components/ProjectPage.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h2>Here is a list of projects I have created</h2>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
